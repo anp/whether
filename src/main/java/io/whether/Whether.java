97ae26ad3ac0f9f0677b07950e0b00212ef942f4
Adam Perry <dikaiosune@users.noreply.github.com>
2015-05-19 23:54:28 -0700

    Using configuration file.

--- src/main/java/io/whether/Whether.java
+++ src/main/java/io/whether/Whether.java
@@ -1,12 +1,22 @@
 package io.whether;
 
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
 import java.io.File;
 
 class Whether {
 
     public static void main(String[] args) {
-        File stationDataFile = new File(args[0]);
-        StationDataParser parser = new StationDataParser(stationDataFile);
-        parser.parse();
+        try {
+            Configuration config = new PropertiesConfiguration(args[0]);
+
+            File stationDataFile = new File((String) config.getProperty("gsodFolderPath") + File.separatorChar + "isd-history.txt");
+            StationDataParser parser = new StationDataParser(stationDataFile);
+            parser.parse();
+        } catch (ConfigurationException ce) {
+            //TODO log error
+        }
     }
 }
